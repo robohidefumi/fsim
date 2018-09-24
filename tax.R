55c8b7e6337c745a73d5ee125f32828cd3097911
Hidefumi (Robo) Watanabe <robo.hidefumi.watanabe@gmail.com>
2018-09-24 16:10:23 +0900

    create pattern

--- tax.R
+++ tax.R
@@ -1,94 +1,19 @@
 library("rlist")
 library("dplyr")
 
-#対象年度設定
-y <- c(0:45 + 2019)
-#年齢設定
-## 生年
-f <- 1974
-yu <- 1977
-m <- 2009
-## 年齢計算
-fumi_a <- y - f
-yuko_a <- y - yu
-michi_a <- y - m
-
-#投資設定
-coupon_share <- 0.7 #利回りのうち利息収入の割合
-#生命保険受け取り
-ins_day1 <- 0.95
-ins_yield <- 0.0438
-ins <- 5.1
-
-df_inheritance <- data.frame(
-  year = y,
-  f_age = fumi_a,
-  y_age = yuko_a,
-  m_age = michi_a
-)
-
-#海外居住年度入力
-###秀文
-##ずっと日本
-#rep("j",46)
-##秀道21歳までSG
-#c(rep("s",11),rep("j",35))
-##ずっとSG
-fumi_l <- rep("s",46)
-###秀道
-##ずっと日本
-#rep("j",46)
-##秀道21歳までSG
-michi_l <- c(rep("s",12),rep("j",34))
-##ずっとSG
-#rep("s",46)
-df_inheritance <- cbind(df_inheritance,f_living=fumi_l)
-df_inheritance <- cbind(df_inheritance,m_living=michi_l)
-##保険所有権設定
-df_inheritance[,"ins_owner"] <- c(rep("f",11),rep("m",35))
-##投資分贈与設定
-#df_master: マスターのデータフレーム
-#df_args: 設定用データフレーム
-df_args <- data.frame(
-  year = c(2030,2044),
-  donate = c(0.5,2)
-)
+##############関数開始##################
+#### 関数定義
 m_donate <- function(df_master,df_args){
   l <- length(df_args$year)
   df_master[,"inv_donate"] <- rep(0,l)
-  for(i in 1:length(df_args$year)){
-    position <- df_args[i,"year"] - 2019 + 1
-    df_master[position,"inv_donate"] <- df_args[i,"donate"]
+  if(l > 0){
+    for(i in 1:length(df_args$year)){
+      position <- df_args[i,"year"] - 2019 + 1
+      df_master[position,"inv_donate"] <- df_args[i,"donate"]
+    }
   }
   df_master
 }
-
-#### 海外居住による税率設定
-df_inheritance <- df_inheritance %>%
-  mutate(f_inc_tax = case_when(
-    f_living == "j" ~ 0.2,
-    f_living == "s" ~ 0
-  )
-  ) %>%
-  mutate(f_live_flg = case_when(
-    f_living == "j" ~ FALSE,
-    f_living == "s" ~ TRUE
-  )
-  ) %>% 
-  mutate(m_inc_tax = case_when(
-    m_living == "j" ~ 0.2,
-    m_living == "s" ~ 0
-  )
-  ) %>%
-  mutate(m_live_flg = case_when(
-    m_living == "j" ~ FALSE,
-    m_living == "s" ~ TRUE
-  )
-  )
-df_inheritance <- m_donate(df_inheritance,df_args)
-
-yield <- 0.06 # 利回り設定
-ini <- 2.5 #初期資金
 
 func_yield <- function(df,yield){
   for(i in 1:length(y)){
@@ -110,7 +35,7 @@ func_yield <- function(df,yield){
       df[i,"m_gross"] * 
       (1 - coupon_share * df[i,"m_inc_tax"])
     df[i,"last"] <- 
-      df[i,"net"] +  df[i,"begin"] - df[i,"inv_donate"]
+      df[i,"net"] +  df[i,"begin"] - df[i,"expense"] - df[i,"inv_donate"]
     df[i,"m_last"] <-
       df[i,"m_net"] +  df[i,"m_begin"]
     ### 保険贈与フラグ設定
@@ -163,10 +88,159 @@ func_yield <- function(df,yield){
     #### 保険贈与税減算処理
     df[i,"inh_ttl"] <- 
       sum(df[i,"inv_inh"],df[i,"ins_inh"],df[i,"m_last"])
-      - df[i,"ins_don_tax"] - df[i,"inv_don_tax"]
+    - df[i,"ins_don_tax"] - df[i,"inv_don_tax"]
   }
   df
 }
 
-df_inheritance <- func_yield(df_inheritance,yield)
-df_inheritance
+#### 海外居住による税率設定
+set_tax <- function(df){
+  df <- df %>%
+    mutate(f_inc_tax = case_when(
+      f_living == "j" ~ 0.2,
+      f_living == "s" ~ 0
+    )
+    ) %>%
+    mutate(f_live_flg = case_when(
+      f_living == "j" ~ FALSE,
+      f_living == "s" ~ TRUE
+    )
+    ) %>% 
+    mutate(m_inc_tax = case_when(
+      m_living == "j" ~ 0.2,
+      m_living == "s" ~ 0
+    )
+    ) %>%
+    mutate(m_live_flg = case_when(
+      m_living == "j" ~ FALSE,
+      m_living == "s" ~ TRUE
+    )
+    )
+  df
+}
+##############関数終了##################
+
+#対象年度設定
+y <- c(0:45 + 2019)
+#年齢設定
+## 生年
+f <- 1974
+yu <- 1977
+m <- 2009
+## 年齢計算
+fumi_a <- y - f
+yuko_a <- y - yu
+michi_a <- y - m
+
+#投資設定
+coupon_share <- 0.7 #利回りのうち利息収入の割合
+#生命保険受け取り
+ins_day1 <- 0.95
+ins_yield <- 0.0438
+ins <- 5.1
+
+df_common <- data.frame(
+  year = y,
+  f_age = fumi_a,
+  y_age = yuko_a,
+  m_age = michi_a
+)
+
+### 生活費試算
+expense <- c(rep(0,15),rep(0.15,10),rep(0.25,10),rep(0.45,11))
+df_common[,"expense"] <- expense
+
+### Pattern A: 親子双方ずっと日本居住
+fumi_l <- rep("j",46)
+michi_l <- rep("j",46)
+df_a <- cbind(df_common,f_living=fumi_l)
+df_a <- cbind(df_a,m_living=michi_l)
+##保険所有権設定
+df_a[,"ins_owner"] <- rep("f",46)
+##投資贈与設定
+df_a[,"inv_donate"] <- rep(0,46)
+
+### Pattern B: 親はずっとSG居住、子はずっと日本居住
+fumi_l <- rep("s",46)
+michi_l <- rep("j",46)
+df_b <- cbind(df_common,f_living=fumi_l)
+df_b <- cbind(df_b,m_living=michi_l)
+##保険所有権設定
+df_b[,"ins_owner"] <- rep("f",46)
+##投資贈与設定
+df_b[,"inv_donate"] <- rep(0,46)
+
+#パターンC: 親子ともに最初の12年のみSG,あとはずっとJP
+fumi_l <- c(rep("s",12),rep("j",34))
+michi_l <- c(rep("s",12),rep("j",34))
+df_c <- cbind(df_common,f_living=fumi_l)
+df_c <- cbind(df_c,m_living=michi_l)
+##保険所有権設定
+df_c[,"ins_owner"] <- c(rep("f",11),rep("m",35))
+##投資分贈与設定
+#df_master: マスターのデータフレーム
+#df_args: 設定用データフレーム
+df_args <- data.frame(
+  year = c(2030),
+  donate = c(0.5)
+)
+df_c <- m_donate(df_c,df_args)
+
+#パターンD: 親はずっとSG、子は最初の12年のみSG,あとはずっとJP
+fumi_l <- rep("s",46)
+michi_l <- c(rep("s",12),rep("j",34))
+df_d <- cbind(df_common,f_living=fumi_l)
+df_d <- cbind(df_d,m_living=michi_l)
+##保険所有権設定
+df_d[,"ins_owner"] <- c(rep("f",11),rep("m",35))
+##投資分贈与設定
+#df_master: マスターのデータフレーム
+#df_args: 設定用データフレーム
+df_args <- data.frame(
+  year = c(2030),
+  donate = c(0.5)
+)
+df_d <- m_donate(df_d,df_args)
+
+#パターンE: 親はずっとSG、子は9~21歳までと35~45歳SG,その他はJP
+fumi_l <- rep("s",46)
+michi_l <- c(rep("s",12),rep("j",14),rep("s",11),rep("j",9))
+df_e <- cbind(df_common,f_living=fumi_l)
+df_e <- cbind(df_e,m_living=michi_l)
+##保険所有権設定
+df_e[,"ins_owner"] <- c(rep("f",11),rep("m",35))
+##投資分贈与設定
+#df_master: マスターのデータフレーム
+#df_args: 設定用データフレーム
+df_args <- data.frame(
+  year = c(2030,2054),
+  donate = c(0.5,2)
+)
+df_e <- m_donate(df_d,df_args)
+
+#### メインルーチン
+df_a <- set_tax(df_a)
+df_a_4 <- func_yield(df_a,0.04)
+df_a_6 <- func_yield(df_a,0.04)
+df_a_8 <- func_yield(df_a,0.04)
+
+df_b <- set_tax(df_b)
+df_b_4 <- func_yield(df_b,0.04)
+df_b_6 <- func_yield(df_b,0.06)
+df_b_8 <- func_yield(df_b,0.08)
+
+df_c <- set_tax(df_c)
+df_c_4 <- func_yield(df_c,0.04)
+df_c_6 <- func_yield(df_c,0.06)
+df_c_8 <- func_yield(df_c,0.08)
+
+df_d <- set_tax(df_d)
+df_d_4 <- func_yield(df_d,0.04)
+df_d_6 <- func_yield(df_d,0.06)
+df_d_8 <- func_yield(df_d,0.08)
+
+df_e <- set_tax(df_e)
+df_e_4 <- func_yield(df_e,0.04)
+df_e_6 <- func_yield(df_e,0.06)
+df_e_8 <- func_yield(df_e,0.08)
+
