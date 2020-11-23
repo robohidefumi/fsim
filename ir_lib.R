9ca3e47fe8470fb4cf6286af29dd16567c855e14
Hidefumi Watanabe <hidefumi.watanabe@rakuten.com>
2020-11-23 12:17:56 +0900

    Add "Common Stock Outstanding"

--- ir_lib.R
+++ ir_lib.R
@@ -186,3 +186,66 @@ facet_trans_plot <- function(category_v){
   
   return(g)
 }
+
+balance_wf_plot2 <- function(category_v, yyyy_v, y_title){
+  
+  df <- apple %>%
+    filter(category == category_v & yyyy == yyyy_v) %>%
+    select(yyyy,item,value)
+  
+  df <- data.frame(Category = df$item, Value2 = df$value)
+  
+  df <- df %>%
+    mutate(Value = ifelse(Category == "Ending", Value2 * -1 , Value2))
+  
+  levels <- df$Category
+  
+  data1 <- df  %>%
+    mutate(Category = factor(Category, levels = levels),
+           ymin = round(cumsum(Value), 3),
+           ymax = lag(cumsum(Value), default = 0),
+           xmin = c(head(Category, -1), NA),
+           xmax = c(tail(Category, -1), NA),
+           Impact = ifelse(Category %in% c(as.character(df$Category[1]), as.character(df$Category[nrow(df)])),"Balance",
+                           ifelse(Value > 0, "Increase", "Decrease")
+           ))
+  
+  g <- ggplot(data1) +
+    theme_bw()+
+    theme(legend.position = "right", panel.grid = element_blank(), 
+          axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+    labs(y = y_title, x = category_v, title = paste(category_v, yyyy_v, collapse = "_"))
+  
+  w <- 0.4  #use to set width of bars
+  
+  g <- g +
+    geom_rect(aes(xmin = as.integer(Category) - w/2,
+                  xmax = as.integer(Category) + w/2, ymin = ymin, ymax = ymax,
+                  fill = Impact), colour = "black") +
+    scale_x_discrete(limits = levels) +
+    scale_fill_manual(values = (c("Decrease" = "blue", "Increase" = "orange", "Balance" = "black")))
+  
+  g <- g +
+    geom_segment(data = data1[1:(nrow(data1) -1),],aes(x = xmin,
+                                                       xend = xmax,
+                                                       y = ymin,
+                                                       yend = ymin))
+  
+  return(g)
+}
+
+facet_yyyy_plot <- function(category_v,exclude_v,y_title){
+  
+  df <- apple%>% 
+    filter(category == "Common_Stock" & !item %in% exclude_v) %>%
+    select(yyyy,item,value)
+  
+  g <- ggplot(df, aes(yyyy,value)) + geom_bar(stat="identity") + 
+    facet_wrap(~item) +
+    theme_bw() +
+    theme(legend.position = "right", panel.grid = element_blank(), 
+          axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+    labs(y = y_title, x = "Year", title = category_v)
+  
+  return(g)
+}
